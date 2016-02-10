6a04aecf7dbb16cbf1f0cf98ed4adef3d7c4a19f
Yura Samkov <y.samkoff@ukr.net>
2016-02-10 23:47:24 +0200

    Initial commit

---
+++ db/migrate/20160210212904_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :avatar_url
+      t.string :provider
+      t.string :profile_url
+      t.string :uid
+
+      t.timestamps
+    end
+      add_index :users, :uid
+      add_index :users, :provider
+      add_index :users, [:uid, :provider], unique: true
+  end
+end
